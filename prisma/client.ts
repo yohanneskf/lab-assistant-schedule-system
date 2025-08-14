675df2422406096f452c3b9ad52a414f7cbde3fc
john <ybelete490@gmail.com>
2025-08-14 09:15:42 -0400

    last updated

---
+++ prisma/client.ts
@@ -0,0 +1,8 @@
+
+import { PrismaClient } from "../lib/generated/prisma"; //[BECAUSE NO OUTPUT] ==> IF OUTPUT THEN USE PATH
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
